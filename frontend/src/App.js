fdd0cad28f5fe53b1d4fa896bb134d9227947764
Ankur-1717 <ankur.ambar17@gmail.com>
2021-12-13 18:03:18 +0530

    Navbar Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,27 +8,12 @@ import {
   Redirect,
   Switch,
 } from "react-router-dom";
-import { useMoralis } from "react-moralis";
 import ItemPage from "./Item/itemPage";
 
 function App() {
-  const { authenticate, isAuthenticated, user, logout, isAuthenticating } = useMoralis();
-  const loginHandler = (e) => {
-    e.preventDefault();
-      authenticate();
-  };
-  const logoutHandler = (e) => {
-    e.preventDefault();
-    logout();
-  };
-  if (user) {
-    console.log(user.get("ethAddress"));
-  }
   return (
     <React.Fragment>
       <Navbar />
-      {!isAuthenticated && <button disabled={isAuthenticating} onClick={loginHandler}>LOGIN</button>}
-      {isAuthenticated && <button disabled={isAuthenticating} onClick={logoutHandler}>LOGOUT</button>}
       <Router>
         <main>
           <Switch>
--- frontend/src/Navigation/navbar.js
+++ frontend/src/Navigation/navbar.js
@@ -1,14 +1,27 @@
 import React, {useState} from 'react';
 import SearchIcon from '@material-ui/icons/Search';
-import RoomIcon from '@material-ui/icons/Room';
 import ReorderIcon from '@material-ui/icons/Reorder';
 import UserIcon from '@material-ui/icons/AccountCircle';
 import WalletIcon from '@material-ui/icons/AccountBalanceWallet';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import { useMoralis } from "react-moralis";
 import '../App.css';
 import './navbar.css';
 
 function Navbar() {
     const [showLinks, setShowLinks] = useState(true);
+    const { authenticate, isAuthenticated, user, logout, isAuthenticating } = useMoralis();
+    const loginHandler = (e) => {
+        e.preventDefault();
+        authenticate();
+    };
+    const logoutHandler = (e) => {
+        e.preventDefault();
+        logout();
+    };
+    if (user) {
+        console.log(user.get("ethAddress"));
+    }
     return (
         <div className='Navbar'>
             <div className='leftSide'>
@@ -23,9 +36,11 @@ function Navbar() {
             <div className='rightSide'>
                 <button onClick={() => setShowLinks(!showLinks)}><ReorderIcon /></button>
                 <div className='links' id = {showLinks ? 'hidden' : ''}>
-                    <a href='/wishlist'> <RoomIcon /> </a>
-                    <a href='/metamask'><WalletIcon /></a>
-                    <a href='/user'> <UserIcon /></a>
+                    <a href='/wishlist'> <FavoriteIcon /> </a>
+                    <a href='/metamask'> <WalletIcon /> </a>
+                    <a href='/user'> <UserIcon /> </a>
+                    {!isAuthenticated && <button disabled={isAuthenticating} onClick={loginHandler}>Login</button>}
+                    {isAuthenticated && <button disabled={isAuthenticating} onClick={logoutHandler}>Logout</button>}
                 </div>   
             </div>
         </div>
