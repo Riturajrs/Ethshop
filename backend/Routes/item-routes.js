f5bd8fe418848b760296de849d13cdaabb3d8f69
Rituraj Seal <rituraj2001.rs@gmail.com>
2021-12-14 11:37:35 +0530

    backend basic structure added

---
+++ backend/Routes/item-routes.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const { check } = require('express-validator');
+
+const router = express.Router();
---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const HttpError = require("./models/http-error");
+const itemRoutes = require("./Routes/item-routes");
+const userRoutes = require("./Routes/users-routes");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
+
+app.use("/api/items", itemRoutes);
+app.use("api/users", userRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
