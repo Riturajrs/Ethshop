cfaffb86c534ad5dd0172a5ecf7805d14cb23f5a
Rituraj Seal <rituraj2001.rs@gmail.com>
2022-07-14 02:50:06 +0530

    CORS PACKAGE ADDED

--- backend/app.js
+++ backend/app.js
@@ -4,21 +4,13 @@ const mongoose = require("mongoose");
 const HttpError = require("./models/http-error");
 const itemRoutes = require("./Routes/item-routes");
 const userRoutes = require("./Routes/users-routes");
+const CORS = require('cors')
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-
-  next();
-});
+app.use(CORS());
 // For item data handling
 app.use("/api/items", itemRoutes);
 // For user data handling
